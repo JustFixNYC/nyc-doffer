70e0b7af3985ec5ecb58d6cb5f7fda1d1b835339
Atul Varma <varmaa@gmail.com>
2019-09-30 11:08:15 -0400

    Make App class.

--- static/main.tsx
+++ static/main.tsx
@@ -1,13 +1,24 @@
 /// <reference path="../node_modules/preact/dist/preact.d.ts" /> 
 
-window.addEventListener('DOMContentLoaded', () => {
-  console.log("SUP");
+class App extends preact.Component {
+  render() {
+    return (
+      <div>
+        <h1>nyc-doffer</h1>
+        <form method="GET">
+          <label for="address">Address</label>
+          <input type="text" id="address" name="address" />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
 
+window.addEventListener('DOMContentLoaded', () => {
   const el = document.getElementById('app');
 
   if (!el) throw new Error('App container not found');
 
-  const hi = <div>hai2u</div>;
-
-  preact.render(hi, el);
+  preact.render(<App/>, el);
 });
