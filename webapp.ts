7d6af0d199ba9f9d72f76c2790c187908cfa1ec9
Atul Varma <varmaa@gmail.com>
2019-09-30 09:59:08 -0400

    Add some stuff, blah

--- webapp.ts
+++ webapp.ts
@@ -3,6 +3,14 @@ import express from 'express';
 
 const PORT = process.env.PORT || '3000';
 
+class Job {
+  constructor(readonly address: string) {
+    
+  }
+}
+
+const jobs = new Map<string, Job>();
+
 const server = express();
 
 server.use(express.static(path.join(__dirname, 'static')));
