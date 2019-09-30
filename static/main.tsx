d80eb9c877c1dbc444f8788dd40314085d9f81eb
Atul Varma <varmaa@gmail.com>
2019-09-30 11:50:25 -0400

    Do websocket stuff

--- static/main.tsx
+++ static/main.tsx
@@ -1,11 +1,16 @@
 /// <reference path="../node_modules/preact/dist/preact.d.ts" /> 
 
 class App extends preact.Component {
+  handleSubmit(e: Event) {
+    e.preventDefault();
+    alert("TODO: Implement this!");
+  }
+
   render() {
     return (
       <div>
         <h1>nyc-doffer</h1>
-        <form method="GET">
+        <form onSubmit={this.handleSubmit.bind(this)}>
           <label for="address">Address</label>
           <input type="text" id="address" name="address" />
           <input type="submit" value="Submit" />
@@ -19,6 +24,20 @@ window.addEventListener('DOMContentLoaded', () => {
   const el = document.getElementById('app');
 
   if (!el) throw new Error('App container not found');
+
+  const protocol = location.protocol === "http:" ? "ws:" : "wss:";
+  const websocketURL = `${protocol}//${location.host}`;
+  const connection = new WebSocket(websocketURL);
+  connection.onopen = () => {
+    console.log("Connection opened!");
+    connection.send('HALLO');
+  };
+  connection.onerror = (error) => {
+    console.log("Connection error!", error);
+  };
+  connection.onmessage = (e) => {
+    console.log("Connection message!", e.data);
+  };
 
   preact.render(<App/>, el);
 });
--- webapp.ts
+++ webapp.ts
@@ -1,5 +1,7 @@
 import path from 'path';
+import http from 'http';
 import express from 'express';
+import ws from 'ws';
 
 const PORT = process.env.PORT || '3000';
 
@@ -11,11 +13,24 @@ class Job {
 
 const jobs = new Map<string, Job>();
 
-const server = express();
+const app = express();
 
-server.use(express.static(path.join(__dirname, 'static')));
+const server = http.createServer(app);
 
-server.use('/vendor/preact', express.static(path.join(__dirname, 'node_modules', 'preact', 'dist')));
+const wss = new ws.Server({ server });
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.use('/vendor/preact', express.static(path.join(__dirname, 'node_modules', 'preact', 'dist')));
+
+wss.on('connection', ws => {
+  ws.on('message', (message) => {
+    console.log('Got message', message);
+    ws.send(`Hallo ${message}`);
+  });
+
+  ws.send(`Greetings from the server!`);
+});
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}.`);
