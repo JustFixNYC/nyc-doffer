7aabf0306a1c63df6fff0e884ca78b397c89108a
Atul Varma <varmaa@gmail.com>
2019-10-02 15:57:26 -0400

    Sync URL with current address.

--- static/main.tsx
+++ static/main.tsx
@@ -22,8 +22,31 @@ class App extends Component<AppProps, AppState> {
     };
   }
 
+  componentDidMount() {
+    window.addEventListener('popstate', this.syncStateWithCurrentURL.bind(this));
+    this.syncStateWithCurrentURL();
+  }
+
+  syncStateWithCurrentURL() {
+    const params = new URLSearchParams(window.location.search);
+    const address = params.get('address') || '';
+    if (address !== this.state.address) {
+      this.setState({
+        address,
+        submittedAddress: address,
+        currentSubmissionId: this.state.currentSubmissionId + 1
+      });
+    }
+  }
+
   handleSubmit(e: Event) {
     e.preventDefault();
+    const params = new URLSearchParams(window.location.search);
+
+    if (params.get('address') !== this.state.address) {
+      params.set('address', this.state.address);
+      window.history.pushState(null, '', '?' + params.toString());
+    }
     this.setState({
       submittedAddress: this.state.address,
       currentSubmissionId: this.state.currentSubmissionId + 1
@@ -42,7 +65,7 @@ class App extends Component<AppProps, AppState> {
         <h1>nyc-doffer</h1>
         <form onSubmit={this.handleSubmit.bind(this)}>
           <label for="address">Address</label>
-          <input type="text" id="address" name="address" list="address-list" onInput={this.handleInput.bind(this)} />
+          <input type="text" id="address" name="address" list="address-list" onInput={this.handleInput.bind(this)} value={this.state.address} />
           <GeoDatalist id="address-list" address={this.state.address} />
           <input type="submit" value="Submit" disabled={isFormDisabled} />
         </form>
