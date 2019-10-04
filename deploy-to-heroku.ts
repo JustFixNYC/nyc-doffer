2097eb0baae0e1df917a2d49e71dd6f6c26efc7a
Atul Varma <varmaa@gmail.com>
2019-10-04 11:31:44 -0400

    Use ts-command-line for deploy-to-heroku tool.

--- deploy-to-heroku.ts
+++ deploy-to-heroku.ts
@@ -1,60 +1,115 @@
 import child_process from 'child_process';
+import { CommandLineParser, CommandLineStringParameter, CommandLineAction, CommandLineFlagParameter } from '@microsoft/ts-command-line';
 
-class Deployer {
+class DeployToHeroku extends CommandLineAction {
+  private _appName?: CommandLineStringParameter;
+  private _ignoreCache?: CommandLineFlagParameter;
+
+  constructor() {
+    super({
+      actionName: 'deploy',
+      summary: 'Deploy the app to Heroku.',
+      documentation: 'This command builds the app container and deploys it to Heroku.'
+    });
+  }
+
+  protected onDefineParameters() {
+    this._appName = this.defineStringParameter({
+      parameterShortName: '-a',
+      parameterLongName: '--app',
+      argumentName: 'APP_NAME',
+      description: 'The Heroku app name to deploy to.',
+      required: true
+    });
+
+    this._ignoreCache = this.defineFlagParameter({
+      parameterLongName: '--ignore-cache',
+      description: 'Do not pull existing container image or use it as a cache.'
+    });
+  }
+
+  protected async onExecute() {
+    const appName = (this._appName && this._appName.value) || '';
+    const ignoreCache = this._ignoreCache && this._ignoreCache.value;
+
+    const heroku = new HerokuDeployer(appName);
+
+    heroku.loginToRegistry();
+    if (ignoreCache) {
+      heroku.docker.build().push();
+    } else {
+      heroku.docker
+        .pull()
+        .build(`${heroku.containerTag}:latest`)
+        .push();
+    }
+    heroku.releaseContainer();
+  }
+}
+
+class DeployerCommandLine extends CommandLineParser {
+  constructor() {
+    super({
+      toolFilename: 'deployer',
+      toolDescription: 'Build and/or deploy nyc-doffer.'
+    });
+
+    this.addAction(new DeployToHeroku());
+  }
+
+  protected onDefineParameters() {
+  }
+}
+
+class DockerBuilder {
+  constructor(readonly containerTag: string) {
+  }
+
+  pull() {
+    runSync(`docker pull ${this.containerTag}`);
+    return this;
+  }
+
+  push() {
+    runSync(`docker push ${this.containerTag}`);
+    return this;
+  }
+
+  build(cacheFrom?: string) {
+    const cacheFromArg = cacheFrom ? `--cache-from ${cacheFrom}` : ``;
+    runSync(`docker build ${cacheFromArg} -t ${this.containerTag} .`);
+    return this;
+  }
+}
+
+class HerokuDeployer {
   readonly processType: string = 'web';
+  readonly docker: DockerBuilder;
 
   constructor(readonly herokuAppName: string) {
+    this.docker = new DockerBuilder(this.containerTag);
   }
 
   get containerTag() {
     return `registry.heroku.com/${this.herokuAppName}/${this.processType}`;
   }
 
-  private runSync(cmdline: string) {
-    console.log(`$ ${cmdline}`)
-    child_process.execSync(cmdline, {stdio: 'inherit'});
-  }
-
   loginToRegistry() {
-    this.runSync(`heroku container:login`);
-    return this;
-  }
-
-  pullFromRegistry() {
-    this.runSync(`docker pull ${this.containerTag}`);
-    return this;
-  }
-
-  pushToRegistry() {
-    this.runSync(`docker push ${this.containerTag}`);
-    return this;
-  }
-
-  buildContainer() {
-    this.runSync(`docker build --cache-from ${this.containerTag}:latest -t ${this.containerTag} .`);
+    runSync(`heroku container:login`);
     return this;
   }
 
   releaseContainer() {
-    this.runSync(`heroku container:release -a ${this.herokuAppName} ${this.processType}`);
+    runSync(`heroku container:release -a ${this.herokuAppName} ${this.processType}`);
     return this;
   }
 }
 
+function runSync(cmdline: string) {
+  console.log(`$ ${cmdline}`)
+  child_process.execSync(cmdline, {stdio: 'inherit'});
+}
+
 if (!module.parent) {
-  const herokuAppName = process.argv[2];
-
-  if (!herokuAppName) {
-    console.log(`usage: deploy-to-heroku <heroku app name>`);
-    process.exit(1);
-  }
-
-  const deployer = new Deployer(herokuAppName);
-
-  deployer
-    .loginToRegistry()
-    .pullFromRegistry()
-    .buildContainer()
-    .pushToRegistry()
-    .releaseContainer();
+  (new DeployerCommandLine()).execute();
 }
