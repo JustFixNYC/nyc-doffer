bbcc1cdf0059cdc72d179b59d98f080b1e1fecad
Atul Varma <varmaa@gmail.com>
2019-10-26 08:19:43 -0400

    Add basic NYCDB integration (#7)
    This adds a 'test_nycdb_connection' command and a NYCDB_URL environment variable.

--- dbtool.ts
+++ dbtool.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import docopt from 'docopt';
-import { getDatabase } from './lib/db';
+import { databaseConnector, nycdbConnector } from './lib/db';
 
 dotenv.config();
 
@@ -9,11 +9,13 @@ const VERSION = '0.0.1';
 const DOC = `
 Usage:
   dbtool.js test_connection
+  dbtool.js test_nycdb_connection
   dbtool.js -h | --help
 `;
 
 type CommandOptions = {
-  test_connection: boolean
+  test_connection: boolean;
+  test_nycdb_connection: boolean;
 };
 
 async function main() {
@@ -21,11 +23,24 @@ async function main() {
 
   if (options.test_connection) {
     await testConnection();
+  } else if (options.test_nycdb_connection) {
+    await testNycdbConnection();
   }
 }
 
+async function testNycdbConnection() {
+  const nycdb = nycdbConnector.get();
+  const table = 'hpd_registrations';
+  const bbls: {count: number} = await nycdb.one(`SELECT COUNT(DISTINCT bbl) FROM ${table};`);
+
+  console.log(`Found ${Intl.NumberFormat().format(bbls.count)} unique BBLs in ${table} table.`);
+  console.log(`Your NYCDB connection seems to be working!`);
+
+  await nycdb.$pool.end();
+}
+
 async function testConnection() {
-  const db = getDatabase();
+  const db = databaseConnector.get();
 
   const tables = await db.any('SELECT * FROM pg_tables;');
 
--- lib/db.ts
+++ lib/db.ts
@@ -1,19 +1,27 @@
 import pgPromise from 'pg-promise';
 
-let db: pgPromise.IDatabase<{}>|null = null;
+class DbSingleton {
+  private db: pgPromise.IDatabase<{}>|null = null;
 
-export function getDatabase() {
-  if (!db) {
-    const DATABASE_URL = process.env.DATABASE_URL;
-
-    if (!DATABASE_URL) {
-      throw new Error('Please define DATABASE_URL in your environment!');
-    }
-
-    const pgp = pgPromise({});
-
-    db = pgp(DATABASE_URL);
+  constructor(readonly envVar: string) {
   }
 
-  return db;
+  get() {
+    if (!this.db) {
+      const url = process.env[this.envVar];
+
+      if (!url) {
+        throw new Error(`Please define ${this.envVar} in your environment!`);
+      }
+
+      const pgp = pgPromise({});
+      this.db = pgp(url);
+    }
+
+    return this.db;
+  }
 }
+
+export const databaseConnector = new DbSingleton('DATABASE_URL');
+
+export const nycdbConnector = new DbSingleton('NYCDB_URL');
