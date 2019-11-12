4931b93ea2d65711d628b07f0598a152dcacb21c
Atul Varma <varmaa@gmail.com>
2019-11-12 05:55:45 -0500

    Sum all rentstab counts in a SOA. Fixes #3.

--- lib/extract-rentstab-units.ts
+++ lib/extract-rentstab-units.ts
@@ -1,7 +1,12 @@
-const REGEX = /(?:Housing-Rent Stabilization|Rent Stabilization Fee- Chg)\s+(\d+)/i;
+const REGEX = /(?:Housing-Rent\s+Stabilization|Rent\s+Stabilization\s+Fee-\s+Chg)\s+(\d+)/i;
 
 export function extractRentStabilizedUnits(text: string): number|null {
-  const match = text.match(REGEX);
-  if (!match) return null;
-  return parseInt(match[1]);
+  let total = 0;
+  for (let line of text.split('\n')) {
+    const match = line.match(REGEX);
+    if (match) {
+      total += parseInt(match[1]);
+    }
+  }
+  return total === 0 ? null : total;
 }
