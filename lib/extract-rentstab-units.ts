49e013a2ab897368ef8f35f75d937da0486c7525
Atul Varma <varmaa@gmail.com>
2019-11-12 06:00:48 -0500

    Use re.exec() instead of iterating through all the lines.

--- lib/extract-rentstab-units.ts
+++ lib/extract-rentstab-units.ts
@@ -1,12 +1,14 @@
-const REGEX = /(?:Housing-Rent\s+Stabilization|Rent\s+Stabilization\s+Fee-\s+Chg)\s+(\d+)/i;
-
 export function extractRentStabilizedUnits(text: string): number|null {
+  const re = /(?:Housing-Rent\s+Stabilization|Rent\s+Stabilization\s+Fee-\s+Chg)\s+(\d+)/i;
   let total = 0;
-  for (let line of text.split('\n')) {
-    const match = line.match(REGEX);
+  let match: RegExpExecArray|null = null;
+
+  do {
+    match = re.exec(text);
     if (match) {
       total += parseInt(match[1]);
     }
-  }
+  } while (match);
+
   return total === 0 ? null : total;
 }
