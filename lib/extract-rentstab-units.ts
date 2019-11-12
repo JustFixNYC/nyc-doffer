40ef3c69a8f46d4845ca31a20da9f595aa6671d4
Atul Varma <varmaa@gmail.com>
2019-11-12 06:23:45 -0500

    Sum all rentstab counts in a SOA (#10)
    Fixes #3.

--- lib/extract-rentstab-units.ts
+++ lib/extract-rentstab-units.ts
@@ -1,7 +1,14 @@
-const REGEX = /(?:Housing-Rent Stabilization|Rent Stabilization Fee- Chg)\s+(\d+)/i;
-
 export function extractRentStabilizedUnits(text: string): number|null {
-  const match = text.match(REGEX);
-  if (!match) return null;
-  return parseInt(match[1]);
+  const re = /(?:Housing-Rent\s+Stabilization|Rent\s+Stabilization\s+Fee-\s+Chg)\s+(\d+)/ig;
+  let total = 0;
+  let match: RegExpExecArray|null = null;
+
+  do {
+    match = re.exec(text);
+    if (match) {
+      total += parseInt(match[1]);
+    }
+  } while (match);
+
+  return total === 0 ? null : total;
 }
---
+++ test/extract-rentstab-units.test.ts
@@ -0,0 +1,44 @@
+import { expect } from 'chai';
+import { extractRentStabilizedUnits } from '../lib/extract-rentstab-units';
+
+// Taken from the 240 1ST AVE. (BBL 1009720001) SOA from June 5, 2019.
+const MULTI_RENTSTAB_TEXT = `
+Rent Stabilization Fee- Chg      96             01/01/2020  50350000            $960.00
+
+Rent Stabilization Fee- Chg      95             01/01/2020  50350100            $950.00
+                                      Statement Details                                                               June 1, 2019
+
+                                                                                                             Bpp st Owner LLC
+
+                                                                                                                      240 1st Ave.
+
+                                                                                                             1-00972-0001
+
+                                                                                                                      Page  5
+
+Tax Year Charges Remaining        Activity Date                 Due Date                                              Amount
+
+Rent  Stabilization  Fee-  Chg    105                           01/01/2020  50350200                         $1,050.00
+
+Rent  Stabilization  Fee-  Chg    105                           01/01/2020  50350300                         $1,050.00
+`;
+
+const MULTI_RENTSTAB_TOTAL = 96 + 95 + 105 + 105;
+
+describe('extractRentStabilizedUnits()', () => {
+  const textAndUnits: [string, number|null][] = [
+    ['Rent Stabilization Fee- Chg      103 01/01/2020', 103],
+    ['Housing-Rent Stabilization       15', 15],
+    ['blahh', null],
+  ];
+
+  textAndUnits.forEach(([text, units]) => {
+    it(`should return ${JSON.stringify(units)} for ${JSON.stringify(text)}`, () => {
+      expect(extractRentStabilizedUnits(text)).to.eq(units);
+    });
+  });
+
+  it(`should combine multiple rent stab charges into a single unit count`, () => {
+    expect(extractRentStabilizedUnits(MULTI_RENTSTAB_TEXT)).to.eq(MULTI_RENTSTAB_TOTAL);
+  });
+});
