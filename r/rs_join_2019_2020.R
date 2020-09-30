f53c4e1df9613349846d380609c6abfb9d30ffbc
Sam R <sam@macbook-pro.home>
2020-09-30 13:19:28 -0400

    refactored replacement of na values

--- r/rs_join_2019_2020.R
+++ r/rs_join_2019_2020.R
@@ -18,10 +18,8 @@ rs_20_cleaned <- rs_20 %>%
 
 # Join data together, using a full_join to include all data from both tables
 rs_joined <- full_join(rs_19_cleaned, rs_20_cleaned, by = "ucbbl") %>% 
-  # replace all NA values with the empty string (so it's SQL friendly)
-  mutate(across(everything(), ~replace_na(.x, ""))) %>%
   # filter out any bbls without rs units either year
   filter(uc2018 > 0 | uc2019 > 0)
 
 # Write CSV for export:
-write_csv(rs_joined, "rentstab_v2.csv")
+write_csv(rs_joined, "rentstab_v2.csv", na = "")
